a3961c45028fb724e089ef2f145981e4bb0afbc9
Du Jingwu <djw@t-chip.com.cn>
2014-09-11 08:57:39 +0800

    support symlink to run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-#! /usr/bin/python
+#! /usr/bin/python2
 # -*- coding: utf-8 -*
 
 
 import sys
 
 import os
-RKFLASHKIT_PATH = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'src')
+RKFLASHKIT_PATH = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'src')
 
 
 if __name__ == '__main__':
